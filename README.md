c21ec18f752cd0d58d00cfb4ff6d0c87608fb628
Quan Bui Minh <buiminhquan.hcmus@gmail.com>
2025-05-16 12:49:37 +0700

    feat: add start and setup functions
    docs: update README.md

--- README.md
+++ README.md
@@ -1,3 +1,64 @@
 File Assistant
 ---
 Read and modify your files with the help of an AI assistant.
+
+The program uses Local MCP server with Azure OpenAI API to interact with your files.
+
+# Usage
+```bash
+> python main.py -h   
+usage: fa [-h] {setup,start} ...
+
+Read and modify your files with the help of an AI assistant.
+
+options:
+  -h, --help     show this help message and exit
+
+commands:
+  {setup,start}
+    setup        Setup program variables
+    start        Start the program
+```
+
+Example of listing files in the specified directory:
+```bash
+> python main.py start
+Please specify the working directory: /your/folder/in/absolute/path
+Working directory set to: '/your/folder/in/absolute/path'
+Secure MCP Filesystem Server running on stdio
+Allowed directories: [ '/your/folder/in/absolute/path' ]
+
+(Say 'thank you' to exit)
+Your message: list the file names in the current folder
+AI response: In the current folder, the following files are present:
+
+- 001_Favorite_Sports.txt
+- 002_Books_about_AI.txt
+- 003_How_to_Grow_Trees.txt
+```
+
+
+
+# Installation
+## Requirements
+1. `Python` >= 3.12
+2. `npx` >= 10.2
+3. A model deployment on Azure OpenAI (or Azure AI Foundry)
+
+## Setup
+Run the following command to setup your program:
+
+```bash
+> python main.py setup
+Please provide the endpoint for your model: https://your-endpoint.cognitiveservices.azure.com/
+Please provide the API version (default: '2024-12-01-preview'): 
+Please provide the name of the deployment: gpt-4o-mini
+Please provide your API key: 
+Please provide the model name: gpt-4o-mini
+Setup successfully!
+```
+
+# Todos
+
+- [ ] Add OpenAI API support
+- [ ] Add conversation history when interacting with the assistant
--- main.py
+++ main.py
@@ -1,2 +1,148 @@
+import sys
+import asyncio
+import os
+import shutil
+import argparse
+import getpass
+from dotenv import load_dotenv
+from mcp import McpError
+from openai import AsyncAzureOpenAI
+from agents import Agent, Runner, ModelProvider, Model, OpenAIChatCompletionsModel, \
+    RunConfig, ModelSettings, set_tracing_disabled
+from agents.mcp import MCPServer, MCPServerStdio
+
+set_tracing_disabled(disabled=True)
+load_dotenv()
+
+
+client = AsyncAzureOpenAI(
+    api_version=os.environ.get("AOAI_API_VERSION"),
+    azure_endpoint=os.environ.get("AOAI_ENDPOINT"),
+    azure_deployment=os.environ.get("AOAI_DEPLOYMENT"),
+    api_key=os.environ.get("AOAI_API_KEY"),
+)
+
+
+class AzureOpenAIModelProvider(ModelProvider):
+    def get_model(self, model_name: str | None) -> Model:
+        return OpenAIChatCompletionsModel(model=model_name or os.environ.get("AOAI_MODEL_NAME"),
+                                          openai_client=client)
+        
+def ai_print(message: str):
+    print(f"AI response: {message}")
+
+
+async def run(mcp_server: MCPServer):
+    agent = Agent(
+        name="Assistant",
+        instructions="""Use the tools to read the files in the given directory, 
+answer questions based on those files, 
+and interact with the files following the user's command.
+""",
+        mcp_servers=[mcp_server],
+    )
+
+    run_config = RunConfig(model_provider=AzureOpenAIModelProvider(),
+                           model_settings=ModelSettings(
+                               temperature=0.9, max_tokens=1000),
+                           )
+    print("\n(Say 'thank you' to exit)")
+    while True:
+        user_input = input("Your message: ")
+        message = user_input.strip()
+        if message == "thank you":
+            ai_print("Goodbye!")
+            break
+
+        result = await Runner.run(starting_agent=agent, input=message,
+                                  run_config=run_config)
+
+        ai_print(f"{result.final_output}\n\n")
+
+
+async def main():
+    workdir = input("Please specify the working directory: ")
+    print(f"Working directory set to: '{workdir}'")
+
+    async with MCPServerStdio(
+            name="Filesystem Server, via npx",
+            params={
+                "command": "npx",
+                "args": [
+                    "-y",
+                    "@modelcontextprotocol/server-filesystem",
+                    workdir
+                ],
+            },
+    ) as server:
+        await run(server)
+
+
+def start(args):
+    try:
+        asyncio.run(main())
+    except McpError as e:
+        ai_print("I cannot access the specified directory. Please check and try again.")
+        sys.exit(2)
+    except Exception as e:
+        ai_print(f"An error occurred, please try again. Error: {e}")
+        sys.exit(2)
+
+
+def setup(args):
+    default_api_version = "2024-12-01-preview"
+    endpoint = None
+    api_version = None
+    deployment = None
+    platform = "azure" # input("Please specify your AI platform ('openai' or 'azure'): ")
+    if platform == "openai":
+        pass
+    elif platform == "azure":
+        endpoint = input("Please provide the endpoint for your model: ")
+        api_version = input(
+            f"Please provide the API version (default: '{default_api_version}'): ")
+        deployment = input("Please provide the name of the deployment: ")
+    else:
+        raise ValueError("Invalid platform. Please try again.")
+    api_key = getpass.getpass("Please provide your API key: ")
+    model_name = input("Please provide the model name: ")
+
+    file_content = f"""
+AI_PLATFORM={platform or ''}
+API_VERSION={api_version or default_api_version}
+API_KEY={api_key or ''}
+AOAI_ENDPOINT={endpoint or ''}
+AOAI_DEPLOYMENT={deployment or ''}
+MODEL_NAME={model_name or ''}
+""".strip()
+
+    env_dir = os.path.dirname(os.path.abspath(__file__))
+    env_path = os.path.join(env_dir, ".env")
+    with open(env_path, "w") as file:
+        file.write(file_content)
+
+    print("Setup successfully!")
+    sys.exit(0)
+
+
 if __name__ == "__main__":
-    print("Hello world!")
+    # Make sure the user has `npx` installed
+    if not shutil.which("npx"):
+        raise RuntimeError(
+            "npx is not installed. Please install it with `npm install -g npx`.")
+
+    parser = argparse.ArgumentParser(
+        prog="fa",
+        description="Read and modify your files with the help of an AI assistant.",
+    )
+    commands = parser.add_subparsers(title="commands")
+    setup_cmd = commands.add_parser("setup", help="Setup program variables")
+    setup_cmd.set_defaults(func=setup)
+    start_cmd = commands.add_parser("start", help="Start the program")
+    start_cmd.set_defaults(func=start)
+    args = parser.parse_args()
+
+    try:
+        args.func(args)
+    except Exception as e:
+        parser.print_help()
